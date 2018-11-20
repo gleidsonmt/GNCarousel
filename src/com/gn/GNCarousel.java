d64da83f02be03a4eb4f7f0a8b6ce18de63cd1a6
Gleidson <gleidisonmt@gmail.com>
2018-11-19 22:13:09 -0200

    initialize repositore

---
+++ src/com/gn/GNCarousel.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright (C) Gleidson Neves da Silveira
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.gn;
+
+import javafx.beans.DefaultProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
+import javafx.scene.layout.Region;
+import javafx.scene.shape.Rectangle;
+
+/**
+ * @author Gleidson Neves da Silveira | gleidisonmt@gmail.com
+ * Create on  18/11/2018
+ * Version 1.0
+ */
+
+@DefaultProperty("children")
+public class GNCarousel extends Region {
+
+    private final static String USER_AGENT_STYELESHEET = GNCarousel.class.getResource("/com/gn/carousel.css").toExternalForm();
+
+    private GNContainer carousel;
+
+    public GNCarousel(){
+        super();
+        carousel = new GNContainer();
+        this.getChildren().add(carousel);
+
+        this.setPrefWidth(300);
+        this.setPrefHeight(300);
+
+        // add clip
+        Rectangle outputClip = new Rectangle();
+        outputClip.setArcWidth(0);
+        outputClip.setArcHeight(0);
+        carousel.setClip(outputClip);
+
+        carousel.layoutBoundsProperty().addListener((ov, oldValue, newValue) -> {
+            outputClip.setWidth(newValue.getWidth());
+            outputClip.setHeight(newValue.getHeight());
+        });
+
+        this.widthProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                carousel.setPrefWidth(newValue.doubleValue());
+            }
+        });
+        this.heightProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                carousel.setPrefHeight(newValue.doubleValue());
+            }
+        });
+
+        carousel.items.addListener(new ListChangeListener<Node>() {
+            @Override
+            public void onChanged(Change<? extends Node> c) {
+                if(carousel.currentView.getChildren().isEmpty()){
+                    carousel.currentView.getChildren().add(carousel.items.get(0));
+                }
+            }
+        });
+    }
+
+    /**
+     * Items to visualize.
+     * @return items.
+     */
+    public ObservableList<Node> getItems(){
+        return carousel.items;
+    }
+
+    @Override
+    public String getUserAgentStylesheet() {
+        return USER_AGENT_STYELESHEET;
+    }
+
+    public void setTitle(String title){
+        carousel.title.setText(title);
+    }
+
+    public void setSubtitle(String sub){
+        carousel.subtitle.setText(sub);
+    }
+}
---
+++ src/sample/App.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) Gleidson Neves da Silveira
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package sample;
+
+import com.gn.GNCarousel;
+import com.gn.GNContainer;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import org.scenicview.ScenicView;
+
+/**
+ * @author Gleidson Neves da Silveira | gleidisonmt@gmail.com
+ * Create on  18/11/2018
+ * Version 1.0
+ */
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        GNCarousel carousel = new GNCarousel();
+        carousel.setTitle("Carousel");
+        carousel.setSubtitle("This is a subtitle.");
+        carousel.getItems().setAll(createItems());
+        Scene scene = new Scene(new StackPane(carousel), 600, 400);
+        stage.setScene(scene);
+        stage.show();
+
+        ScenicView.show(scene);
+
+    }
+
+    private ObservableList<Node> createItems(){
+
+        Label lb1 = new Label("First");
+        Label lb2 = new Label("Second");
+        Label lb3 = new Label("Third");
+        Label lb4 = new Label("Fourth");
+        Label lb5 = new Label("Fifth");
+
+        lb1.setStyle("-fx-text-fill : white; -fx-font-size : 24px;");
+        lb2.setStyle("-fx-text-fill : white; -fx-font-size : 24px;");
+        lb3.setStyle("-fx-text-fill : white; -fx-font-size : 24px;");
+        lb4.setStyle("-fx-text-fill : white; -fx-font-size : 24px;");
+        lb5.setStyle("-fx-text-fill : white; -fx-font-size : 24px;");
+
+        VBox v1 = new VBox(lb1);
+        VBox v2 = new VBox(lb2);
+        VBox v3 = new VBox(lb3);
+        VBox v4 = new VBox(lb4);
+        VBox v5 = new VBox(lb5);
+
+        v1.setAlignment(Pos.CENTER);
+        v2.setAlignment(Pos.CENTER);
+        v3.setAlignment(Pos.CENTER);
+        v4.setAlignment(Pos.CENTER);
+        v5.setAlignment(Pos.CENTER);
+
+        v1.setStyle("-fx-background-color : #FF3547;");
+        v2.setStyle("-fx-background-color : #512DA8;");
+        v3.setStyle("-fx-background-color : #48CFAD;");
+        v4.setStyle("-fx-background-color : #02C852;");
+        v5.setStyle("-fx-background-color : #EC407A;");
+
+        return FXCollections.observableArrayList(v1, v2, v3, v4, v5);
+    }
+}
